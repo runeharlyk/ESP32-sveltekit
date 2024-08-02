f7499b80a6095a200365ca9a43c86a7a9107adfe
Rune Harlyk <rune@harlyk.dk>
2024-08-02 18:22:46 +0200

    Removes unused function

--- interface/src/routes/system/metrics/SystemMetrics.svelte
+++ interface/src/routes/system/metrics/SystemMetrics.svelte
@@ -241,33 +241,6 @@
 		temperatureChart.data.datasets[0].data = $analytics.core_temp;
 		temperatureChart.update('none');
 	}
-
-	function convertSeconds(seconds: number) {
-		// Calculate the number of seconds, minutes, hours, and days
-		let minutes = Math.floor(seconds / 60);
-		let hours = Math.floor(minutes / 60);
-		let days = Math.floor(hours / 24);
-
-		// Calculate the remaining hours, minutes, and seconds
-		hours = hours % 24;
-		minutes = minutes % 60;
-		seconds = seconds % 60;
-
-		// Create the formatted string
-		let result = '';
-		if (days > 0) {
-			result += days + ' day' + (days > 1 ? 's' : '') + ' ';
-		}
-		if (hours > 0) {
-			result += hours + ' hour' + (hours > 1 ? 's' : '') + ' ';
-		}
-		if (minutes > 0) {
-			result += minutes + ' minute' + (minutes > 1 ? 's' : '') + ' ';
-		}
-		result += seconds + ' second' + (seconds > 1 ? 's' : '');
-
-		return result;
-	}
 </script>
 
 <SettingsCard collapsible={false}>
